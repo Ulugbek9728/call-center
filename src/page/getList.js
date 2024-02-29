537099a5ae86eff2cfcb28d10b8186cfd4580860
Ulugbek <ulugbekshukurov9728@gmail.com>
2024-02-29 17:15:33 +0500

    fileALL

--- src/page/getList.js
+++ src/page/getList.js
@@ -1,5 +1,19 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Input, Space, Steps, Table, Modal, Skeleton, Collapse, theme, Segmented, Upload, Button, message} from "antd";
+import {
+    Input,
+    Space,
+    Steps,
+    Table,
+    Modal,
+    Skeleton,
+    Collapse,
+    theme,
+    Segmented,
+    Upload,
+    Button,
+    message,
+    Select
+} from "antd";
 import {LoadingOutlined, CaretRightOutlined, UploadOutlined} from "@ant-design/icons";
 import axios from "axios";
 import {ApiName} from "../APIname";
@@ -27,6 +41,7 @@ const getItems = (panelStyle) => [
         style: panelStyle,
     },
 ];
+
 function GetList(props) {
     const componentRef = useRef();
     const handlePrint = useReactToPrint({content: () => componentRef.current,});
@@ -57,27 +72,33 @@ function GetList(props) {
             id: "string",
             name: "string"
         },
-        exchangeType: "SEND",
+        exchangeType: "BACK",
         description: "string",
-        files: [
-        ]
+        files: []
 
     });
     const [Datee, setDatee] = useState();
-    const [value, setValue] = useState('Javob berish');
+    const [Department, setDepartment] = useState([]);
 
 
     useEffect(() => {
-        const options = {day: '2-digit', month: '2-digit', year: 'numeric'};
-
-        const formattedDate = new Date(ariza?.createdDate).toLocaleDateString('en-US', options);
+        const options = {day: '2-digit', month: '2-digit', year: 'numeric'}
+        const formattedDate = new Date(ariza?.createdDate).toLocaleDateString('en-US', options)
         setDatee(formattedDate)
-
-
     }, [Open]);
 
     const handleOk = () => {
-        setOpen(false);
+        // setOpen(false);
+        console.log(arizaSend)
+        axios.post(`${ApiName}/api/v1/exchange-application`, arizaSend, {
+            headers: {"Authorization": `Bearer ${fulInfo.accessToken}`}
+        }).then((response) => {
+            console.log(response);
+
+        }).catch((error) => {
+            console.log(error)
+
+        })
     };
 
     const onSearch = (value, _e, info) => {
@@ -86,7 +107,22 @@ function GetList(props) {
 
     useEffect(() => {
         arizaGetList()
+        DepartmenGet()
     }, []);
+
+    useEffect(() => {
+        if (arizaSend.exchangeType==="BACK"){
+            setArizaSend({...arizaSend,
+                toDepartment:{
+                id: ariza?.exchangesApp ? ariza?.exchangesApp[ariza?.exchangesApp?.length - 1]?.department?.id : null,
+                name: ariza?.exchangesApp ? ariza?.exchangesApp[ariza?.exchangesApp?.length - 1]?.department?.name : null,
+
+                }
+
+            })
+        }
+
+    }, [arizaSend.exchangeType]);
 
     function arizaGetList() {
         axios.get(`${ApiName}/api/application`, {
@@ -127,18 +163,21 @@ function GetList(props) {
             title: "seeAll",
             render: (item, record, index) => (
                 <button className='btn btn-outline-success' onClick={(e) => {
-                    console.log(item);
-                    setArizaSend({...arizaSend,
-                        applicationId:item.id})
+                    setArizaSend({
+                        ...arizaSend,
+                        applicationId: item.id,
+                        toDepartment: item.exchangesApp[item.exchangesApp.length - 1].department
+                    })
                     setAriza(item)
+                    console.log(item)
+                    arizaFileList(item.id)
                     setOpen(true)
                 }}>
                     See
                 </button>),
         },
     ];
-
-    const { token } = theme.useToken();
+    const {token} = theme.useToken();
     const panelStyle = {
         marginBottom: 24,
         background: "#c3e7ff",
@@ -168,9 +207,7 @@ function GetList(props) {
                     console.log(error)
                     message.error(`${info.file.name} file delete failed.`);
                 })
-            }
-
-            else if (info.file.status === 'done') {
+            } else if (info.file.status === 'done') {
                 arizaSend.files.push({
                         fileId: info.file.response.id,
                     }
@@ -182,6 +219,35 @@ function GetList(props) {
         },
     };
 
+    function DepartmenGet() {
+        axios.get(`https://api-id.tdtu.uz/api/department?structureCode=ALL`, {}).then((response) => {
+            setDepartment(response.data);
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
+    const handleChangeDepartme = (e) => {
+        const result = Department.filter((word) => word.id === e);
+        setArizaSend({...arizaSend,
+            toDepartment: {
+            id: result[0].id,
+            name: result[0].name,
+            }})
+    };
+
+    function arizaFileList(id) {
+        axios.get(`${ApiName}/api/v1/exchange-application/files-by-application`, {
+            headers: {"Authorization": `Bearer ${fulInfo?.accessToken}`},
+            params:{
+                appId:id
+            }
+
+        }).then((response) => {
+            console.log(response.data.data)
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
 
     return (
         <div>
@@ -248,13 +314,14 @@ function GetList(props) {
                     {/*    })}*/}
 
                     {/*</ul>*/}
+
                     <div className="w-50 px-4">
 
                         <div className="collapseAll border">
                             <Collapse
                                 bordered={false}
                                 defaultActiveKey={['1']}
-                                expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+                                expandIcon={({isActive}) => <CaretRightOutlined rotate={isActive ? 90 : 0}/>}
                                 style={{
                                     background: "white",
                                 }}
@@ -262,13 +329,58 @@ function GetList(props) {
                             />
                         </div>
 
-                        <Segmented options={["Javob berish", "Boshqa bo'limga o'tkazish"]} value={value} onChange={setValue} block/>
+                        <Segmented
+                            options={[
+                                {
+                                    label: "Javob berish",
+                                    value: "BACK"
+                                },
+                                {
+                                    label: "Boshqa bo'limga o'tkazish",
+                                    value: "SEND"
+                                },
+                            ]}
+                            value={arizaSend.exchangeType}
+                            onChange={(e) => {
+                                setArizaSend({
+                                    ...arizaSend,
+                                    exchangeType: e
+                                })
+                            }} block/>
+
+                        {arizaSend.exchangeType==='BACK'? '': <div className="my-3">
+                            <label form="ID" className="form-label">Markaz / Bo'lim / Fakultet / Kafedra ga
+                                yuborish</label>
+                            <br/>
+                            <Select className='w-100'
+                                    showSearch
+                                    value={arizaSend.toDepartment?.name}
+
+                                    onChange={(e) => {
+                                        handleChangeDepartme(e)
+                                    }}
+                                    placeholder="Markaz / Bo'lim / Fakultet / Kafedra"
+                                    optionFilterProp="children"
+
+                                    filterOption={(input, option) => (option?.label?.toLowerCase() ?? '').startsWith(input.toLowerCase())}
+                                    filterSort={(optionA, optionB) =>
+                                        (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                                    }
+                                    options={Department && Department.map((item, index) => ({
+                                        value: item.id,
+                                        label: item.name
+                                    }))}
+                            />
+                        </div>}
+
+
+
                         <label htmlFor="comment" className='mt-2'>Javob mazmuni:</label>
                         <textarea className="form-control mt-2" rows="6" id="comment" name="text"
 
-                                  // onChange={(e) => {
-                                  //     setAriza({...ariza, description: e.target.value})
-                                  // }}
+                                  onChange={(e) => {
+                                      setArizaSend({...arizaSend, description: e.target.value})
+                                  }}
                         />
                         <Upload {...propss}>
                             <Button icon={<UploadOutlined/>}>Click to Upload</Button>
@@ -276,8 +388,6 @@ function GetList(props) {
                     </div>
 
                 </div>
-
-
 
 
             </Modal>
